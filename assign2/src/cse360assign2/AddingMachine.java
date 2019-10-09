0c37aab540b7327fa8d2e37e27ddec7e0dcc52a3
ShihYuChang <schang72@asu.edu>
2019-10-09 16:25:11 -0700

    Initial checkin of cse360assign2

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,35 @@
+/**
+ * @author      Shih Yu Chang
+ * @classID     2019Fall-T-CSE360-70641 <https://canvas.asu.edu/courses/27412>                                 
+ * @assignmentNumber 2
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
