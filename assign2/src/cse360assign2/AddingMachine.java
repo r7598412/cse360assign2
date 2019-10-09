5e2225a74df8f44fe33821f1bc94cb086f502a17
ShihYuChang <schang72@asu.edu>
2019-10-09 16:32:35 -0700

    Change methods getTotal() and add(). Add the variable "tos".

--- assign2/src/cse360assign2/AddingMachine.java
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -8,17 +8,22 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String tos = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	// return total value
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	// add value to total and save the transcation
 	public void add (int value) {
-		
+		total = total + value;
+		String str = Integer.toString(value);
+		tos = tos + " + " + str;
 	}
 	
 	public void subtract (int value) {
